9bbc48463e97ed37ef0b1096ab883aa45d0bea47
Bram van der Veen <96aa48@gmail.com>
2013-08-24 02:28:41 +0200

    Made the build.bat script better.

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
-7z a -mcl -tzip app.nw -r */*.*
-7z a -mcl -tzip app.nw -r */*
-7z a -mcl -tzip app.nw -r */*/*.*
-7z a -mcl -tzip app.nw -r *
+rm app.nw
+7z a -mcl -tzip app.nw /r /u *
 
 C:/Development/webdevelopment/nodejs/node-webkit/nw.exe app.nw
